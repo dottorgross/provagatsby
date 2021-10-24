9c24279175da4d471fc22147496532c52e15ebb0
Andrea Ros <a.ros@donboscosandona.it>
2021-10-24 17:06:04 +0000

    Finisched Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Il mio sito con Gatsby",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
